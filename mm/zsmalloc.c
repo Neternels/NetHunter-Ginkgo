Neternels/NetHunter-Ginkgo

commit b92239a8cd40d6cded7aa3aed3b7a6f6af89a6e2

zsmalloc: add compaction and huge class callbacks
Add compaction callbacks for zpool compaction API extension.
Add huge_class_size callback too to be fully aligned.

With these in place, we can proceed with ZRAM modification
to use the universal (zpool) API.

Change-Id: Ia240bdf5de0a7aafbb5e5c906115587999f08b4e
Signed-off-by: Vitaly Wool <vitalywool@gmail.com>
Signed-off-by: Alex Winkowski <dereference23@outlook.com>
Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

mm/zsmalloc.c
@@ -439,9 +439,27 @@ static void zs_zpool_unmap(void *pool, unsigned long handle)
 	zs_unmap_object(pool, handle);
 }
 
+static unsigned long zs_zpool_compact(void *pool)
+{
+	return zs_compact(pool);
+}
+
+static unsigned long zs_zpool_get_compacted(void *pool)
+{
+	struct zs_pool_stats stats;
+
+	zs_pool_stats(pool, &stats);
+	return stats.pages_compacted;
+}
+
 static u64 zs_zpool_total_size(void *pool)
 {
 	return zs_get_total_pages(pool) << PAGE_SHIFT;
+}
+
+static size_t zs_zpool_huge_class_size(void *pool)
+{
+	return zs_huge_class_size(pool);
 }
 
 static struct zpool_driver zs_zpool_driver = {
@@ -454,7 +472,10 @@ static struct zpool_driver zs_zpool_driver = {
 	.shrink =	zs_zpool_shrink,
 	.map =		zs_zpool_map,
 	.unmap =	zs_zpool_unmap,
+	.compact =	zs_zpool_compact,
+	.get_num_compacted = zs_zpool_get_compacted,
 	.total_size =	zs_zpool_total_size,
+	.huge_class_size = zs_zpool_huge_class_size,
 };
 
 MODULE_ALIAS("zpool-zsmalloc");

commit 81f6132e6bb58e615e8bb8e92420309c007b9c92

zsmalloc: pass atomic64_t to return type for `zs_zpool_get_compacted()`
Fixes: b92239a {"zsmalloc: add compaction and huge class callbacks"}
Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

mm/zsmalloc.c
@@ -449,7 +449,7 @@ static unsigned long zs_zpool_get_compacted(void *pool)
 	struct zs_pool_stats stats;
 
 	zs_pool_stats(pool, &stats);
-	return stats.pages_compacted;
+	return atomic_long_read(&stats.pages_compacted);
 }
 
 static u64 zs_zpool_total_size(void *pool)